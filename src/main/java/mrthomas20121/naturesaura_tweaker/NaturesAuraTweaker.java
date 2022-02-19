d498c6905d46d34fdfd26a92313f19e2c6dc0de5
Mrthomas20121 <thomas77300@gmail.com>
2022-02-19 07:27:47 +0100

    Base mod class

---
+++ src/main/java/mrthomas20121/naturesaura_tweaker/NaturesAuraTweaker.java
@@ -0,0 +1,13 @@
+package mrthomas20121.naturesaura_tweaker;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(NaturesAuraTweaker.mod_id)
+public class NaturesAuraTweaker {
+
+	public static final String mod_id = "naturesaura_tweaker";
+
+	public NaturesAuraTweaker() {
+
+	}
+}
