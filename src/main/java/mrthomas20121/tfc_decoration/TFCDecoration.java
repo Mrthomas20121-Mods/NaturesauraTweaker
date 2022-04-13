61f91ce970ee8e78ef85800d3fca3d9672ad21cf
Mrthomas20121 <thomas77300@gmail.com>
2022-04-13 09:15:34 +0200

    Main java class

---
+++ src/main/java/mrthomas20121/tfc_decoration/TFCDecoration.java
@@ -0,0 +1,13 @@
+package mrthomas20121.tfc_decoration;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(TFCDecoration.mod_id)
+public class TFCDecoration {
+
+	public static final String mod_id = "tfc_decoration";
+
+	public TFCDecoration() {
+
+	}
+}
